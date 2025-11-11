8fc6c865dd7850effa5880e040ba44e47d50b909
Leonie von Schirach <leonie.schirach@web.de>
2025-11-11 15:43:41 +0000

    add query to solve task 3

--- queries.sql
+++ queries.sql
@@ -13,3 +13,13 @@ FROM dim_name
 JOIN fact_staff ON dim_name.staff_id = fact_staff.staff_id
 ORDER BY cohorts_worked DESC
 LIMIT 1;
+
+-- Task 3: Which quarter is the busiest for our staff members?
+SELECT 
+    EXTRACT(QUARTER FROM dim_date.next_cohort_date) AS quarter, 
+    SUM(fact_staff.mentees) AS total_mentees 
+FROM fact_staff
+JOIN dim_date ON dim_date.date_id = fact_staff.date_id
+GROUP BY quarter
+ORDER BY total_mentees DESC
+LIMIT 1;
