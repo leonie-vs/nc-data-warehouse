5e005bba93794a6f1da527b100a4310e6a73abea
Leonie von Schirach <leonie.schirach@web.de>
2025-11-11 15:25:08 +0000

    complete second query task

--- queries.sql
+++ queries.sql
@@ -5,3 +5,11 @@ SELECT dim_name.full_name FROM dim_name
 JOIN fact_staff ON dim_name.staff_id = fact_staff.manager_id
 ORDER BY fact_staff.mentees_staff_rating
 LIMIT 1;
+
+-- Task 2: Which staff member has worked the most cohorts based on how much they charge 
+-- per event and the total revenue to far?
+SELECT dim_name.full_name, ROUND(CAST(revenue / daily_rate AS numeric),0) AS cohorts_worked
+FROM dim_name
+JOIN fact_staff ON dim_name.staff_id = fact_staff.staff_id
+ORDER BY cohorts_worked DESC
+LIMIT 1;
