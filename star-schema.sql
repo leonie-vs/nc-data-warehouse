e23aa04e12d08c2c703974befa4cb98f76dbc743
Leonie von Schirach <leonie.schirach@web.de>
2025-11-11 13:23:02 +0000

    create and populate fact staff table

--- star-schema.sql
+++ star-schema.sql
@@ -5,7 +5,7 @@
 
 
 -- cohort date dimension table
-DROP TABLE IF EXISTS dim_date;
+DROP TABLE dim_date CASCADE;
 
 CREATE TABLE dim_date
 (
@@ -21,7 +21,7 @@ SELECT * FROM dim_date;
 
 
 -- area dimension table
-DROP TABLE IF EXISTS dim_area;
+DROP TABLE dim_area CASCADE;
 
 CREATE TABLE dim_area
 (
@@ -37,7 +37,7 @@ SELECT * FROM dim_area;
 
 
 -- name dimension table
-DROP TABLE IF EXISTS dim_name;
+DROP TABLE dim_name CASCADE;
 
 CREATE TABLE dim_name
 (
@@ -54,7 +54,7 @@ SELECT * FROM dim_name;
 
 
 -- course dimension table
-DROP TABLE IF EXISTS dim_course;
+DROP TABLE dim_course CASCADE;
 
 CREATE TABLE dim_course
 (
@@ -68,3 +68,42 @@ FROM staff;
 
 SELECT * FROM dim_course;
 
+
+-- fact table
+DROP TABLE fact_staff CASCADE;
+
+CREATE TABLE fact_staff 
+(
+    staff_id INT REFERENCES dim_name(staff_id),
+    course_id INT REFERENCES dim_course(course_id),
+    area_id INT REFERENCES dim_area(area_id),
+    date_id INT REFERENCES dim_date(date_id),
+    manager_id INT REFERENCES dim_name(staff_id),
+    mentees INT,
+    mentees_staff_rating INT,
+    daily_rate FLOAT,
+    revenue FLOAT
+);
+
+INSERT INTO fact_staff (
+    staff_id, area_id, course_id, manager_id, date_id, mentees, 
+    mentees_staff_rating, daily_rate, revenue
+)
+SELECT 
+    empl.staff_id, 
+    dim_area.area_id, 
+    dim_course.course_id, 
+    manager.staff_id AS manager_id, 
+    dim_date.date_id, 
+    staff.mentees, 
+    staff.mentees_staff_rating,
+    staff.daily_rate, 
+    staff.revenue
+FROM staff
+JOIN dim_name empl ON staff.first_name = empl.first_name AND staff.last_name = empl.last_name
+JOIN dim_area ON staff.area = dim_area.area
+JOIN dim_course ON staff.course = dim_course.course
+JOIN dim_name manager ON staff.manager = manager.first_name || ' ' || manager.last_name
+JOIN dim_date ON TO_DATE(staff.next_cohort_date, 'DD Month YYYY') = dim_date.next_cohort_date;
+
+SELECT * FROM fact_staff;
