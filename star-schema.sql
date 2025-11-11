a63d155ccf2fbbb90a62fe72fb6dedc51aa1bf0b
Leonie von Schirach <leonie.schirach@web.de>
2025-11-11 12:15:06 +0000

    create and populate area dimension table

--- star-schema.sql
+++ star-schema.sql
@@ -3,9 +3,9 @@
 -- facts table: mentees, mentees_staff_rating, daily_rate, revenue
 -- dimensions: first_name, last_name, area, course, manager, next_cohort_date
 
+-- cohort date dimension table
 DROP TABLE IF EXISTS dim_date;
 
--- cohort date dimension table
 CREATE TABLE dim_date
 (
     date_id SERIAL PRIMARY KEY,
@@ -17,3 +17,18 @@ SELECT DISTINCT TO_DATE(next_cohort_date, 'DD Month YYYY')
 FROM staff; 
 
 SELECT * FROM dim_date;
+
+-- area dimension table
+DROP TABLE IF EXISTS dim_area;
+
+CREATE TABLE dim_area
+(
+    area_id SERIAL PRIMARY KEY,
+    area VARCHAR(50)
+);
+
+INSERT INTO dim_area (area)
+SELECT DISTINCT area
+FROM staff;
+
+SELECT * FROM dim_area;
